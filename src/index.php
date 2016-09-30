3c11cd921fb2590ce4597a357bab4e247c93e790
Oskari Saari <okkuweb@gmail.com>
2016-10-01 00:22:56 +0300

    Finished the damn build system... Added a html build so I won't have to build the entire thing on small changes

--- src/index.php
+++ src/index.php
@@ -20,7 +20,7 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <p>Hello world! This is HTML5.</p>
+        <p>Hello world! This is Oskari :j.</p>
 
         <script src="js/bundle.js"></script>
     </body>
