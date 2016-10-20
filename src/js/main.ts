ec5d6eeb51680ce13eb567e7f8d0bf96940c26e3
Oskari Saari <okkuweb@gmail.com>
2016-10-20 18:46:15 +0300

    Animation performance optimization

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,9 +1,9 @@
 const header = document.getElementById("header__large")
 
 setInterval(function() {
-    if(window.scrollY === 0){
+    if(window.scrollY <= 5){
         header.id = "header__large";
-    } else if(window.scrollY !== 0) {
+    } else if(window.scrollY > 5) {
         header.id = "header__small";
     }
 }, 200);
